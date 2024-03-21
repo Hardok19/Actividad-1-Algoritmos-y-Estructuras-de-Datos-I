70975824cadf2e343d63c0e5cf6bfce964bb9e78
Hardok19 <harold21092003@gmail.com>
2024-03-21 01:47:02 -0600

    a

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -25,22 +25,64 @@ public class LinkedList<T> {
             current.next = newNode;
         }
     }
-// Other methods for LinkedList implementation...
+
+    public void insertarlista(LinkedList Alist) {
+        Node currentA = Alist.head;
+        Node<T> current = head;
+        if (head != null) {
+            while (current.next != null) {
+                current = current.next;
+            }
+            while (currentA != null) {
+                current.next = currentA;
+                currentA = currentA.next;
+                current = current.next;
+            }
+        }
+        else {
+            head = currentA;
+            currentA = currentA.next;
+            while (currentA != null) {
+                current = currentA;
+                currentA = currentA.next;
+                current = current.next;
+            }
+
+        }
 
 
-    public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<>();
-        list.add("Hello");
-        list.add("World");
-        list.add("Java");
-        LinkedList<Integer> numbers = new LinkedList<>();
-        numbers.add(1);
-        numbers.add(2);
-        LinkedList<Double> decimals = new LinkedList<>();
-        decimals.add(3.14);
-        decimals.add(2.71);
-        System.out.println(list);
+    }
+    public void imprimir(){
+        Node<T> current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
     }
 
 
+
+
+        public static void main (String[]args){
+            LinkedList<String> list = new LinkedList<>();
+            list.add("Hello");
+            list.add("World");
+            list.add("Java");
+            LinkedList<Integer> numbers = new LinkedList<>();
+            numbers.add(1);
+            numbers.add(2);
+            LinkedList<Double> decimals = new LinkedList<>();
+            decimals.add(3.14);
+            decimals.add(2.71);
+            LinkedList<Double> dec = new LinkedList<>();
+
+
+
+            dec.insertarlista(list);
+            decimals.insertarlista(dec);
+            decimals.imprimir();
+        }
+
+
 }
+
